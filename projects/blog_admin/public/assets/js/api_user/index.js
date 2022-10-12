526bb1d6681ac35f527827541f9c1366676280bd
Roman <roman@rtacademy.net>
2022-10-12 12:33:08 +0300

    projects

--- projects/blog_admin/public/assets/js/api_user/index.js
+++ projects/blog_admin/public/assets/js/api_user/index.js
@@ -1,4 +1,12 @@
 "use strict";
+
+const renderActionsCell = ( data, type, row, meta ) =>
+`
+    <a href="/api/user/view/${row.id}" class="btn" title="View"><i class="uil-eye"></i></a>
+    <a href="/api/user/edit/${row.id}" class="btn" title="Edit"><i class="uil-pen"></i></a>
+    <a href="#" data-url="/api/user/delete/${row.id}" class="btn action-delete" title="Delete"><i class="uil-trash"></i></a>
+`;
+
 $( document ).ready( function()
 {
     const dataTable = $( "#table" ).DataTable(
@@ -13,8 +21,24 @@ $( document ).ready( function()
                 { data: 'token' },
                 { data: 'created_date', render: ( data ) => moment(data).format( 'DD.MM.YYYY HH:mm:ss' ) },
                 { data: 'status', render: ( data ) => data.charAt(0).toUpperCase() + data.slice(1) },
+                { render: renderActionsCell },
             ],
             keys        : !0
+        }
+    );
+
+    $('#table').on(
+        'click',
+        'a.action-delete',
+        function( e )
+        {
+            e.preventDefault();
+            $.ajax(
+                {
+                    'method'    : 'DELETE',
+                    'url'       : $(this).data('url'),
+                }
+            );
         }
     );
 } );
